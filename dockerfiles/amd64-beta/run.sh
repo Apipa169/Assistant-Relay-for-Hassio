35814c57d2844a952c3894dde86d9e4ae6eb16f6
Apipa169 <44235469+Apipa169@users.noreply.github.com>
2020-02-22 18:54:37 +0100

    Update run.sh

--- dockerfiles/amd64-beta/run.sh
+++ dockerfiles/amd64-beta/run.sh
@@ -3,21 +3,18 @@ PORT=$(jq --raw-output ".port | length" $CONFIG_PATH)
 AUDIORESPONSE="/data/audio-responses"
 CONFIG="/data/config.json"
 
-
 if [ ! -d "$AUDIORESPONSE" ]; then
         echo Creating an audio-responses folder... This needs to be done once. 
         mkdir /data/audio-responses
 fi
 
 if [ -f $CONFIG ]; then
-        echo "File $CONFIG exists."
+        echo "File $CONFIG exists. Setting config."
+        cat config.json|jq --arg port "$PORT" '.port = $port' config.json|less -E
 else
         echo Creating configfile.... 
-        jq -n --arg port "$PORT" "{port:${port}}" > /data/config.json   
+        jq -n --arg port "$PORT" '{port:$port}' > /data/config.json
 fi
-
-
 cd /assistant_relay
 echo Assistant Relay is now starting...
-
 npm run start
